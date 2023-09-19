8f9875d90939d3f23cff82b04ce860f8c9137e9b
MRAJEKO <aiden.kops@outlook.com>
2023-09-19 16:49:42 +0200

    Automatic reconnection

--- frame/index.js
+++ frame/index.js
@@ -2,23 +2,29 @@ var imageContainer = document.getElementById("image");
 var nextImageContainer = document.getElementById("loading");
 var textContainer = document.getElementById("text");
 
-var socket = new WebSocket("ws://192.168.0.111:3000");
+function connectToSocket() {
+  var socket = new WebSocket("ws://192.168.0.111:3000");
 
-socket.onopen = function (event) {
-  console.log("Connected to the server.");
-};
+  socket.onopen = function (event) {
+    console.log("Connected to the server.");
+  };
 
-socket.onmessage = function (event) {
-  console.log(event);
-  var data = JSON.parse(event.data);
+  socket.onmessage = function (event) {
+    console.log(event);
+    var data = JSON.parse(event.data);
 
-  console.log(data.text);
+    textContainer.innerHTML = data.text;
+    imageContainer.src = data.image;
+    nextImageContainer.src = data.nextImage;
+  };
 
-  textContainer.innerHTML = data.text;
-  imageContainer.src = data.image;
-  nextImageContainer.src = data.nextImage;
-};
+  socket.onclose = function (event) {
+    console.log("Connection closed.");
+    textContainer.innerHTML = "Reconnecting...";
+    setTimeout(function () {
+      connectToSocket();
+    }, 2500);
+  };
+}
 
-socket.onclose = function (event) {
-  console.log("Connection closed.");
-};
+connectToSocket();
--- source/index.js
+++ source/index.js
@@ -2,23 +2,31 @@ const imageContainer = document.getElementById("image");
 const nextImageContainer = document.getElementById("loading");
 const textContainer = document.getElementById("text");
 
-const socket = new WebSocket("ws://localhost:3000");
+function connectToSocket() {
+  const socket = new WebSocket("ws://localhost:3000");
 
-socket.addEventListener("open", (event) => {
-  console.log("Connected to the server.");
-});
+  socket.addEventListener("open", (event) => {
+    console.log("Connected to the server.");
+  });
 
-socket.addEventListener("message", (event) => {
-  console.log(event);
-  const { text, image, nextImage } = JSON.parse(event.data);
+  socket.addEventListener("message", (event) => {
+    console.log(event);
+    const { text, image, nextImage } = JSON.parse(event.data);
 
-  console.log(text);
+    console.log(text);
 
-  textContainer.textContent = text;
-  imageContainer.src = image;
-  nextImageContainer.src = nextImage;
-});
+    textContainer.textContent = text;
+    imageContainer.src = image;
+    nextImageContainer.src = nextImage;
+  });
 
-socket.addEventListener("close", (event) => {
-  console.log("Connection closed.");
-});
+  socket.addEventListener("close", (event) => {
+    console.log("Connection closed.");
+    textContainer.textContent = "Reconnecting...";
+    setTimeout(() => {
+      connectToSocket();
+    }, 2500);
+  });
+}
+
+connectToSocket();
