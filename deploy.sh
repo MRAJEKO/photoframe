95fcbf18eef748b28c54faa9f34d294a20c7e5db
MRAJEKO <aiden.kops@outlook.com>
2023-09-21 09:29:35 +0200

    Comments and reset changes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 forever stop server.js
 
+# Remove any changes to the code locally
+git reset --hard HEAD
+
 git pull --force
 
+# Install app packages for the root
 npm i
 
 cd './controlpanel'
 
+# Install all package for the control panel
 npm i
 
 npm run build
 
 cd '..'
 
+# Start forever in the root
 forever start server.js
